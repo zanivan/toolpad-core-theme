c1cdcb2825f80ef5da5ffb481dacca0911627bd4
zanivan <victorzanivan@gmail.com>
2024-09-06 08:20:47 -0300

    First commit

---
+++ app/(dashboard)/orders/page.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+export default function OrdersPage() {
+  return (
+    <Typography>
+      Welcome to the Toolpad orders!
+    </Typography>
+  );
+}
---
+++ app/(dashboard)/page/page.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export default function Home() {
+  return (
+    <main>
+      <Typography>Hello world!</Typography>
+    </main>
+  );
+}
